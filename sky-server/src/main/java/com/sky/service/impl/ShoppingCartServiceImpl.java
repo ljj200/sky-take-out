8f40323a20f481a48e8ebe404ad858c110e864ac
86135 <3057026140@qq.com>
2023-09-07 16:59:10 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -77,4 +77,20 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         }
 
     }
+
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    public List<ShoppingCart> showShoppingCart() {
+        //获取到当前微信用户id
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCat = ShoppingCart.builder()
+                .userId(userId)
+                .build();
+
+        List<ShoppingCart> list =  shoppingCartMapper.list(shoppingCat);
+        return list;
+    }
 }
